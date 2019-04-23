18205eb704dd09fab89406f469b80a212608a84a
Wayne Collier <ke4tri@gmail.com>
2019-04-22 21:17:51 -0500

    connection ADO.Net

--- SwordAndFather/Data/UserRepository.cs
+++ SwordAndFather/Data/UserRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using SwordAndFather.Models;
@@ -23,6 +24,36 @@ namespace SwordAndFather.Data
 
             return newUser;
         }
+
+        public List<User> GetAll()
+        {
+            var users = new List<User>();
+            var connection = new SqlConnection("Server=localhost;Database=SwordAndFather; Trusted_Connection = True;");
+            connection.Open();
+
+            var getAllUsersCommand = connection.CreateCommand();
+            getAllUsersCommand.CommandText = "select * from users";
+
+            var reader = getAllUsersCommand.ExecuteReader();
+
+
+            while (reader.Read())
+            {
+                //returns object not int
+                //var Id = reader[0];
+                //var Id = (int)reader[0];
+                //var Id = reader.GetInt32(0);
+                var id = (int)reader["Id"];
+                var username = reader["username"].ToString();
+                var password = reader["password"].ToString();
+                var user = new User(username, password) { Id = id } ;
+
+                users.Add(user);
+            }
+            connection.Close();
+            return users;
+        }
+            
 
     }
 }
